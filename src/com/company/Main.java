4110451df3a1b2d5619169f504ef0902b12df176
luffyhan <luffyhan@gmail.com>
2021-05-02 08:39:07 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Word, GitHub!!");
+	// edit from GitHub
+	System.out.println("Edit from GitHub!");
     }
 }
